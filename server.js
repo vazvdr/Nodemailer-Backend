3e817035fa94ccf0548083f2c8661dd8df0aaf73
vazvdr <azevedo.vanderson@hotmail.com>
2024-12-28 16:24:54 -0300

    Habilitando o cors para receber requisição de qualquer lugar

--- server.js
+++ server.js
@@ -1,32 +1,37 @@
-require("dotenv").config();
+require("dotenv").config(); // Importa o dotenv no início do arquivo
 const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+const cors = require("cors"); // Importa o cors
 
 const app = express();
-app.use(cors());
+
+// Permite requisições de qualquer origem
+const corsOptions = {
+  origin: "*", // Aceita requisições de qualquer origem
+  methods: ["POST"], // Permite GET e POST
+  allowedHeaders: ["Content-Type"], // Permite cabeçalhos específicos
+};
+
+app.use(cors(corsOptions)); // Adiciona o CORS ao seu servidor
+
 app.use(bodyParser.json());
 
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
-  if (!name || !email || !message) {
-    return res.status(400).send("Todos os campos são obrigatórios.");
-  }
-
   const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: process.env.EMAIL_USER, 
+      user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
 
   const mailOptions = {
-    from: `"${name}" <${process.env.EMAIL_USER}>`, 
+    from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER,
-    subject: `Contato pelo portifólio ${name}`,
+    subject: `Contato pelo portifólio de ${name}`,
     text: message,
     replyTo: email,
   };
@@ -35,12 +40,10 @@ app.post("/send-email", async (req, res) => {
     await transporter.sendMail(mailOptions);
     res.status(200).send("Email enviado com sucesso!");
   } catch (error) {
-    console.error("Erro ao enviar email:", error);
+    console.error(error);
     res.status(500).send("Erro ao enviar email.");
   }
 });
 
-const PORT = 3000;
-app.listen(PORT, () =>
-  console.log(`Servidor rodando na porta ${PORT}.`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
